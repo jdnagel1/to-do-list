d7477c5344577de4f8cb16da97ec5ae31b13e87c
Jake Nagel and Lindsey Baker <lindsey@>
2019-04-08 15:00:24 -0700

    add constructor functions

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// Object containing task and Unique ID's:
+
+function ToDo() {
+  this.tasks = [],
+  this.currentId = 0
+}
+
+
+// Constructor:
+
+function Tasks(task,date,time){
+  this.task = task,
+  this.date = date,
+  this.time = time
+}
